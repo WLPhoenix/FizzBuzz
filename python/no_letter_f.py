dae0bceb738a57c55989bad764561678218af932
cory.hughes <cory.hughes@digitalreasoning.com>
2015-06-15 15:56:37 -0500

    Fine clark, i'll get rid of all the F's

--- python/no_letter_f.py
+++ python/no_letter_f.py
@@ -3,8 +3,8 @@ import sys
 map(sys.stdout.write,
     map(
         lambda n: "%s\n" % {
-            (True, True): "FizzBuzz",
-            (True, 1 == 0): "Fizz",
+            (True, True): "\x46izzBuzz",
+            (True, 1 == 0): "\x46izz",
             (1 == 0, True): "Buzz",
             (1 == 0, 1 == 0): str(n)
         }[(n % 3 == 0, n % 5 == 0)],
